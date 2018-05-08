cd7be1f96703c439274b2084ad2a401295e6583d
J M <jody.markic@gmail.com>
2018-05-08 19:20:38 -0400

    restructured js code, grouped code in functions, removed unused code, created the init function responsible for initialize event handlers.

--- index.js
+++ index.js
@@ -1,69 +1,79 @@
 var numSquares = 6;
-
-var colors = generateRandomColors(numSquares);
+var colors = [];
+var pickedColor;
 
 var squares = document.querySelectorAll(".square");
-
-var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var headerOne = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 
-for(var i = 0; i < modeButtons.length; i++) {
-	modeButtons[i].addEventListener("click", function(){
-		modeButtons[0].classList.remove("selected");
-		modeButtons[1].classList.remove("selected");
-		this.classList.add("selected");
+init();
 
-		this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
-		reset();
-	})
+
+function init() {
+	//mode button event listeners
+	setupModeButtons();
+	setupSquareClicks();
+	setupResetButton();
+	reset();
 }
 
-resetButton.addEventListener("click", function(){
-	//generate all new colors
-	colors = generateRandomColors(numSquares);
-	//pick a new random color from array
-	pickedColor = pickColor();
-	//change colorDisplay to match picked Color
-	colorDisplay.textContent = pickedColor;
 
-	//change colors of squares
-	for(var i =0; i < squares.length; i++) {
-		squares[i].style.backgroundColor = colors[i];
+function setupModeButtons() {
+	for(var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+
+			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			reset();
+		})
 	}
-	resetButton.textContent = "New Colors";
-	headerOne.style.backgroundColor = "steelblue";
-	messageDisplay.textContent = "";
-});
-
-
-colorDisplay.textContent = pickedColor;
-
-for(var i = 0; i < squares.length; i++) {
-	//add initial colors to squares
-	squares[i].style.backgroundColor = colors[i];
-
-	//add click listeneres to squares
-	squares[i].addEventListener("click", function(){
-		//grab color of clicked square
-		var clickedColor = this.style.backgroundColor;
-
-		//compare color to pickedColor
-		if(clickedColor === pickedColor) {
-			messageDisplay.textContent = "Correct!";
-			resetButton.textContent = "Play Again?";
-			changeColors(clickedColor);
-			headerOne.style.backgroundColor = clickedColor;
-		}
-		else {
-			this.style.backgroundColor = "#232323";
-			messageDisplay.textContent = "Try Again"; 
-		}
-	})
 }
+
+function setupResetButton() {
+	resetButton.addEventListener("click", function(){
+		//generate all new colors
+		colors = generateRandomColors(numSquares);
+		//pick a new random color from array
+		pickedColor = pickColor();
+		//change colorDisplay to match picked Color
+		colorDisplay.textContent = pickedColor;
+
+		//change colors of squares
+		for(var i =0; i < squares.length; i++) {
+			squares[i].style.backgroundColor = colors[i];
+		}
+		resetButton.textContent = "New Colors";
+		headerOne.style.backgroundColor = "steelblue";
+		messageDisplay.textContent = "";
+	});	
+}
+
+function setupSquareClicks() {
+	for(var i = 0; i < squares.length; i++) {
+		//add click listeneres to squares
+		squares[i].addEventListener("click", function(){
+			//grab color of clicked square
+			var clickedColor = this.style.backgroundColor;
+			//compare color to pickedColor
+			if(clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct!";
+				resetButton.textContent = "Play Again?";
+				changeColors(clickedColor);
+				headerOne.style.backgroundColor = clickedColor;
+			}
+			else {
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try Again"; 
+			}
+		})
+	}
+}
+
 
 function reset() {
 	colors = generateRandomColors(numSquares);
